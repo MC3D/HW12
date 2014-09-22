90a1048d7ca591a74502b0d4cef128c28c16c378
MC3D <m.chapman.home@gmail.com>
2014-09-22 14:26:04 -0400

    update

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -6,4 +6,42 @@ $.ajaxSetup({
   }
 });
 
-console.log('The Iron Yard Rocks');
+function renderTemplate(templateId, location, model) {
+  var templateString = $(templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(model);
+  $(location).append(renderedTemplate);
+}
+
+
+// setInterval(function(){
+//   $.ajax({
+//     type: "GET",
+//     dataType: "json",
+//     url: "https://api.github.com/issues"
+//   });
+// },2000);
+
+setInterval($.ajax({
+  type: "GET",
+  dataType: "json",
+  url: "https://api.github.com/issues"
+  }).done(function(issues) {
+  _.each(issues, function(issue) {
+    renderTemplate('#open', '#issues', issue);
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: issue.comments_url
+    }).done(function(comments) {
+      _.each(comments, function(comment) {
+        renderTemplate('#text', '#comments', comment);
+      });
+    });
+  });
+}),10000);
+
+// https://api.github.com/repos/TIY-GVL-FEE-2014-Aug/Assignments/issues/149/comments
+
+
+// "https://api.github.com/repos/TIY-GVL-FEE-2014-Aug/Assignments/issues/149/comments"
